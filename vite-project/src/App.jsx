550c805def8057438af506db7d13401928682f27
PAWANPATEL <pp467354@gmail.com>
2024-09-19 14:28:01 +0530

    update

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Footer from './Footer'
+import Header from './Header'
+import Icon from './Icon'
+import Navbar from './Navbar'
+import Section from './Section'
+
+
+function App() {
+  
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    <Icon/>
+    <Section/>
+    <Footer/>
+      
+    </>
+  )
+}
+
+export default App
